5dfeb16652fd7a812d456c8cce4d05efe2bac08e
Andi Liwantoro Susanto <andiliesusanto@gmail.com>
2023-10-16 16:14:20 +0700

    Feat : Installed Tailwind CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function FirstPost() {
     return (
         <div>
-            <h1>
+            <h1 className="text-4xl font-bold underline">
                 First post
             </h1>
             <h2>

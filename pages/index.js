bf0ea2e3416ab002eb0920fcb8009a23db74d857
andiliewantorosusanto <andiliesusanto@gmail.com>
2023-10-16 23:20:29 +0700

    Feat : Added Some Text and _Document for imports

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,34 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
-        </Head>
-        <h1>
-          First Post
-        </h1>
-        <h2>
-          Kentang
-        </h2>
+        <div className="w-screen h-screen flex">
+          <div className="m-auto flex justify-center w-screen">
+            <div className="w-5/12">
+              <h2>
+                Hello! My Name Is
+              </h2>
+              <h6 className="text-6xl font-extrabold">
+                Andi Liwantoro Susanto
+              </h6>
+              <h2 className="text-xl">
+                Application Development Specialist at BCA Finance
+              </h2>
+            </div>
+            <div className="w-4/12 gap-y-2">
+              <p>
+                My first spark in code was when i make a scipt for my favorite game! minecraft. 
+                it was an amazing, a simple text can make something that i can see and play. 
+                After that i knew i want to delve this hobby of mine to build something bigger.
+              </p>
+              <p>
+                Currently, I lead a project that will transform BCA Finance to a better loan company!.
+              </p>
+              <p>
+                I want to have more knowledge to be a better engineer
+              </p>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   )

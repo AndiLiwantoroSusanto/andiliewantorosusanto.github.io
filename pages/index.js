28588f03f0a9d9781a899b50ecde194b937e0c31
Andi Liwantoro Susanto <andiliesusanto@gmail.com>
2023-10-16 18:01:56 +0700

    Feat : Added layout and font family

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import Head from "next/head"
+import Layout from "../components/layout"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Layout>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
+        </Head>
+        <h1>
+          First Post
+        </h1>
+        <h2>
+          Kentang
+        </h2>
+      </Layout>
+    </>
   )
 }
